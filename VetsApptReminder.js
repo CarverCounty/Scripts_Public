78067c0a87c57d2090f997ad318feeec445ab20f
dynamiccookies <dynamiccookies@users.noreply.github.com>
2018-08-07 10:45:47 -0500

    Update VetsApptReminder.js
    Added functionality to clear the Street Address, Email Address, and Other text fields when their respective checkboxes are unchecked.

--- VetsApptReminder.js
+++ VetsApptReminder.js
@@ -1,22 +1,35 @@
 NWF$(document).ready(function () {
 	NWF$('.placeholders').each(function(){
-	   NWF$('#' + this.id).attr("placeholder", NWF$('#' + this.id).attr("title"));
-	});      
+	   NWF$('#' + this.id).attr('placeholder', NWF$('#' + this.id).attr('title'));
+	});
+	NWF$('#' + SendTo + ' input, #' + ApptType + ' input').change(function(){
+		if(NWF$('input#' + this.id).prop('checked')){
+			if(NWF$("label[for='" + this.id + "']").text()=='Street Address'){
+				NWF$('.address').val('');
+			}
+			if(NWF$("label[for='" + this.id + "']").text()=='Email Address'){
+				NWF$('.email').val('');
+			}
+			if(NWF$("label[for='" + this.id + "']").text()=='Other'){
+					NWF$('.other').val('');
+			}
+		}
+	});
 	NWF$('#' + zip).change(function(){
 		$.ajax({
 			type: 'POST',
 			url: 'https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML='+ 
 				'<CityStateLookupRequest USERID="[USPS USER ID]">'+
 				'<ZipCode ID="0"><Zip5>'+$(this).val()+'</Zip5></ZipCode></CityStateLookupRequest>',
-				dataType: 'xml',
-				success: function (response) {
-					$('ZipCode',response).each(function(){ 
-						NWF$('#' + city).val(
-							$(this).find('City').text().toLowerCase().replace(/\b(\S)/g,function(t){return t.toUpperCase()}
-						));
-						NWF$('#' + state).val($(this).find('State').text());
-					});	
-				},	
+			dataType: 'xml',
+			success: function (response) {
+				$('ZipCode',response).each(function(){ 
+					NWF$('#' + city).val(
+						$(this).find('City').text().toLowerCase().replace(/\b(\S)/g,function(t){return t.toUpperCase()}
+					));
+					NWF$('#' + state).val($(this).find('State').text());
+				});
+			},
 			error: function (error){console.log(error);}
 		});
 	});
