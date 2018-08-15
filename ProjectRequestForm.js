46788049a0eb2903cd431379c93fe9155f8cfb87
dynamiccookies <dynamiccookies@users.noreply.github.com>
2018-08-15 11:03:44 -0500

    Update ProjectRequestForm.js
    Expanded CountyUsers definition section to include Less than One Department and All Users options.
    Added descriptive comments for entire script.

--- ProjectRequestForm.js
+++ ProjectRequestForm.js
@@ -1,13 +1,22 @@
 NWF.FormFiller.Events.RegisterAfterReady(function(){
+	/* Array to hold CountyUsers definition for each Division in DepartmentsAffected section */
 	var result = [];
+
+	/* Array to hold all DepartmentsAffected List Lookup ID variables */
 	var depts = [deptHHS,deptPublicServices,deptPublicWorks,deptSheriff,deptER,deptCourt,deptFinance,deptAdmin,deptATNY,deptBoard];
+
+	/* Loop through all DepartmentsAffected List Lookup fields to check for selected departments on form load */
 	for (var x=0;x<depts.length;x++){calcDepts(result,depts[x],depts);}
+
+	/* On SelectAllDepts change, set all Division checkboxes equal to SelectAllDepts. Triggers change to cascade to Departments. */
 	NWF$('#' + SelectAllDepts).change(function() {
 		NWF$('.divs input').prop('checked',NWF$(this).prop('checked')).trigger('change');
 		checkBoxes(deptAdmin,SelectAllDepts);
 		checkBoxes(deptATNY,SelectAllDepts);
 		checkBoxes(deptBoard,SelectAllDepts);
 	});
+
+	/* On any Division change, set its Departments equal to Division checkbox using checkBoxes function. Triggers Department change. */
 	NWF$('.divs input').change(function() {
 		if(this.id == divHHS){checkBoxes(deptHHS,divHHS);}
 		else if(this.id == divPublicServices){checkBoxes(deptPublicServices,divPublicServices);}
@@ -17,38 +26,62 @@ NWF.FormFiller.Events.RegisterAfterReady(function(){
 		else if(this.id == divCourt){checkBoxes(deptCourt,divCourt);}
 		else if(this.id == divFinance){checkBoxes(deptFinance,divFinance);}
 	});
-	NWF$('#'+deptHHS+',#'+deptPublicServices+',#'+deptPublicWorks+',#'+deptSheriff+',#'+deptER+
-		',#'+deptCourt+',#'+deptFinance+',#'+deptAdmin+',#'+deptATNY+',#'+deptBoard).change(function(){
+
+	/* On any Department change, run calcDepts function to determine selected Department checkboxes */
+	NWF$('#' + deptHHS + ',#' + deptPublicServices + ',#' + deptPublicWorks + ',#' + deptSheriff + ',#' + deptER +
+		',#' + deptCourt + ',#' + deptFinance + ',#' + deptAdmin + ',#' + deptATNY + ',#' + deptBoard).change(function(){
 		var thisID = this.id;
 		calcDepts(result,thisID,depts);
 	});
 });
+
+/* Function to calculate selected vs total Departments in current Division and store result into 'CountyUsers' Form Variable*/
 function calcDepts(result,thisid,depts){
-	var allIDs = '';
-	var b;
-	var strCountyUsers = '';
+	var allIDs = '', strCountyUsers = '';
+	
+	/* Number of selected checkboxes in current Division */
 	var checked = NWF$('#' + thisid.replace(/_hid$/g,'')).find('input:checked').length;
+
+	/* Total checkboxes in current Division */
 	var total = NWF$('#' + thisid.replace(/_hid$/g,'')).find('input').length;
-	for(var a in depts){
-		if(NWF$('#' + depts[a]).val()){
-			var arr = NWF$('#' + depts[a]).val().split(';#');
-			for(b=0;b<arr.length;b+=2){allIDs += arr[b]+',';}
+
+	/* For each selected Department in ALL Divisions, split {ID;#Value;#ID;#Value} array to return only IDs as comma delimited string */
+	for(var x in depts){
+		if(NWF$('#' + depts[x]).val()){
+			var arr = NWF$('#' + depts[x]).val().split(';#');
+			for(var y=0;y<arr.length;y+=2){allIDs += arr[y] + ',';}
 		}
 	}
+
+	/* Store allIDs comma delimited string into 'AllIDs' Form Variable with trailing comma trimmed to be stored directly into List*/
 	NWF$("[data-controlname='AllIDs'] input").val(allIDs.replace(/,$/g,''));
+
+	/* Determine CountyUsers definition for current Division and store in result associative array with ID as key */
 	if(checked == total){result[thisid] = 'One Division';}
 	else if(checked == 1){result[thisid] = 'One Department';}
 	else if(checked == 0){delete result[thisid];}
 	else {result[thisid] = 'Multiple Departments';}
-	if(arrSize(result)>1){strCountyUsers = 'Multiple Divisions';}
+	
+	/* Determine CountyUsers definition for All Divisions and store in strCountyUsers string */
+	if(NWF$('#' + SelectAllDepts).prop('checked')){strCountyUsers = 'All Users';}
+	else if(arrSize(result)>1){strCountyUsers = 'Multiple Divisions';}
 	else{strCountyUsers = result[Object.keys(result)[0]];}
+	if(!strCountyUsers && {Common:IsEditMode}) {strCountyUsers = 'Less than One Department';}
+
+	/* Set 'CountyUsers' Form Variable equal to strCountyUsers string */
 	NWF$("[data-controlname='CountyUsers'] input").val(strCountyUsers);
-	console.dir(checked + " of " + total + " checkboxes checked in current division = " + result[thisid]);
-	console.dir("Number of Divisions: " + arrSize(result) + " - Overall Rating: " + NWF$("[data-controlname='CountyUsers'] input").val());	
+
+	/* Output checked, total, current Division's CountyUsers definition, # of Divisions with selections, and final definition to console */
+	console.dir(checked + ' of ' + total + ' checkboxes checked in current division = ' + result[thisid]);
+	console.dir('Number of Divisions: ' + arrSize(result) + ' - Overall Rating: ' + NWF$("[data-controlname='CountyUsers'] input").val());	
 }
+
+/* Function used on SelectAllDepts and Division changes to set individual Department checkbox values. Triggers change to run calcDepts */
 function checkBoxes(dept,div){
 	NWF$('#' + dept.replace(/_hid$/g,'') + ' input').prop('checked',NWF$('#' + div).prop('checked')).trigger('change');
 }
+
+/* Function to calculate size of associative array */
 function arrSize(arr){
 	var size = 0, key;
 	for(key in arr){if(arr.hasOwnProperty(key)) size++;}
