69bc2a1dfa328496173b5231ded8271188956a02
Chad Davis <dynamiccookies@users.noreply.github.com>
2018-09-07 10:02:00 -0500

    Update RemoveDropDownDuplicates.js

--- RemoveDropDownDuplicates.js
+++ RemoveDropDownDuplicates.js
@@ -1,10 +1,10 @@
 NWF.FormFiller.Events.RegisterAfterReady(function(){
-	$('.duplicates').each(function(){Update(this.id);});
+	NWF$('.duplicates').each(function(){Update(this.id);});
 });
 function Update(control){
 	var usednames={};
-	$("select[id="+control+"]>option").each(function(){
-		if(usednames[this.text]){$(this).remove();}            
-		else{usednames[this.text]=this.value;}   
+	NWF$("select[id="+control+"]>option").each(function(){
+		if(usednames[this.text]){NWF$(this).remove();}
+		else{usednames[this.text]=this.value;}
 	});
 }
