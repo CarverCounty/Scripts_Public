10f47ed2499bd0acb61bf91a042a86a5241520c7
Chad Davis <cdavis@co.carver.mn.us>
2019-04-04 10:52:27 -0500

    Moved to Scripts Repo
    Moved the queryString.js file from Simpe-Query-String repo to scripts repo and deleted old repo.

---
+++ SimpleQueryString.js
@@ -0,0 +1,23 @@
+﻿/*Requires a reference to jQuery*/
+/*qs will be an object with all query strings
+EXAMPLE: http://<someurl>/?x=-93.25&y=44.56&zoomFactor=8
+qs = {
+    x: -93.25,
+    y: 44.56,
+    zoomFactor: 8
+}
+*/
+var qs = getQS(); 
+function getQS() {
+    var url = window.location.toString();
+    var query_string = url.split("?");
+    var qsparamslist = {};
+    if (query_string.length > 1) {
+        var qsparams = query_string[1].split("&");
+        $.each(qsparams, function (index, val) {
+            var param_item = val.split("=");
+            qsparamslist[param_item[0]] = param_item[1];
+        });
+    }
+    return qsparamslist;
+}
