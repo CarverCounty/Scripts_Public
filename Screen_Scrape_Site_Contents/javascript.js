e8a8ae23ebb27eb045a97d0932ad97362ad257d8
Chad Davis <cdavis@co.carver.mn.us>
2019-05-21 16:28:38 -0500

    Initial Commit of Screen_Scrape_Site_Contents
    This is a basic solution to be used in JSFiddle. When used, it will parse the contents of a SharePoint 2013 Site Contents page into a list of the Lists, Libraries, and Sites to be easily copied and pasted into a Word or OneNote file.

---
+++ Screen_Scrape_Site_Contents/javascript.js
@@ -0,0 +1,49 @@
+/* Enter Your Domain Here */
+var myDomain = '';
+
+/* Hide copied code from Site Contents in results window */
+$('table#appsTable').css('display','none');
+
+/* Add bold title to lists and libraries section on the right */
+$('#siteContents').append('<p></p><strong>LISTS & LIBRARIES</strong><br><br>');
+
+/* Find each list and library... */
+$('div[id].ms-vl-appinfo').each(function(){
+
+	/* Store the data in variables using jQuery */
+	var linkData = $('#' + this.id + ' .ms-vl-apptitleouter a.ms-vl-apptitle');
+	var metaData = $('#' + this.id + ' div.ms-vl-appstatus');
+  
+	/* Append a link and metadata in a list to the section on the right */	
+	$('#siteContents').append(
+
+		/* Create the list item */
+		'<li>' + 
+    	/* Create the link */
+    	'<a href=\'' + myDomain + 
+      	linkData.prop('href').replace('https://fiddle.jshell.net/','') + 
+      	'\' target=\'_blank\'>' + 
+      		linkData.text().trim() + 
+    	'</a> - ' + 
+			
+      /* Format the metadata */
+			metaData[0].innerHTML.trim() + ' - ' + 
+      metaData[1].innerHTML.trim().replace('Modified','') + 
+      
+    /* Close the list item */
+    '</li>'
+  );
+});
+
+/* Add bold title to sites section */
+$('#siteContents').append('<p>&nbsp;</p><p>&nbsp;</p><strong>SITES</strong><br><br>');
+
+/* Find each site, build a link, and add it to the page like above */
+$('.ms-itmhover .ms-vb-icon a.ms-draggable').each(function(){
+	$('#siteContents').append('<li><a href=\'' + 
+  	$(this).prop('href') + 
+    '\' target=\'_blank\'>' + 
+    $(this).text().trim() + 
+    '</a></li>'
+  );
+});
